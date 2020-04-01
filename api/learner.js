607a9c9d75d26b178213083c665d55b259920bed
ball <nattawatx86@gmail.com>
2020-04-02 03:17:02 +0700

    register & login

--- api/learner.js
+++ api/learner.js
@@ -1,37 +1,73 @@
-const router = require('express').Router()
+const router = require("express").Router()
 const LearnerUser = require("../model/LearnerUser")
-const bcrypt = require('bcrypt')
-const jwt = require('jwt-simple')
+const bcrypt = require("bcrypt")
+const jwt = require("jwt-simple")
 
 //Learner
 router.get("/api/learners", (req, res) => {
-    LearnerUser.find({}, function(err, users) {
-          res.send({ users: users })
-    })
+      LearnerUser.find({}, function(err, users) {
+            res.send({ users: users })
+      })
 })
 
 router.get("/api/learner/:id", (req, res) => {
-    LearnerUser.findOne({ _id: req.params.id }, function(err, user) {
-          if (user) {
-                res.send(user)
-          } else {
-                res.status(400).send("not found user")
-          }
-    })
+      LearnerUser.findOne({ _id: req.params.id }, function(err, user) {
+            if (user) {
+                  res.send(user)
+            } else {
+                  res.status(400).send("not found user")
+            }
+      })
 })
 
-router.get("/auth/learner/login" ,(req, res) => {
+router.get("/auth/learner/login", (req, res) => {
       LearnerUser.findOne({ username: req.body.username }, function(err, user) {
-            if(user) {
-                  bcrypt.compare(req.body.password, user.password, function(err, res1) {
-                        if(res1) {
-                              res.send(jwt.encode(req.body.username,"MY_SECRET_KEY"))
+            if (user) {
+                  bcrypt.compare(req.body.password, user.password, function(
+                        err,
+                        res1
+                  ) {
+                        if (res1) {
+                              res.send(
+                                    jwt.encode(
+                                          req.body.username,
+                                          "MY_SECRET_KEY"
+                                    )
+                              )
                         } else {
-                              res.sendStatus(401)           
+                              res.sendStatus(401)
                         }
-                    })
+                  })
             } else {
                   res.sendStatus(401)
+            }
+      })
+})
+
+router.post("/auth/learner/signup", (req, res) => {
+      LearnerUser.find({}, function(err, result) {
+            if (
+                  result.filter(
+                        learner =>
+                              learner.username === req.body.username ||
+                              learner.email === req.body.email
+                  ).length === 0
+            ) {
+                  let instance = new LearnerUser({
+                        username: req.body.username,
+                        password: req.body.password,
+                        email: req.body.email,
+                        subjectID: []
+                  })
+                  instance.save(function(err2, result2) {
+                        if (err2 || !result2) {
+                              res.sendStatus(400)
+                        } else {
+                              res.send("signup success")
+                        }
+                  })
+            } else {
+                  res.status(400).send("duplicate username or password")
             }
       })
 })
--- api/teacher.js
+++ api/teacher.js
@@ -1,36 +1,72 @@
-const router = require('express').Router()
+const router = require("express").Router()
 const TeacherUser = require("../model/TeacherUser")
-const bcrypt = require('bcrypt')
+const bcrypt = require("bcrypt")
 
 //Teacher
 router.get("/api/teachers", (req, res) => {
-    TeacherUser.find({}, function(err, users) {
-          res.send({ users: users })
-    })
+      TeacherUser.find({}, function(err, users) {
+            res.send({ users: users })
+      })
 })
 
 router.get("/api/teacher/:id", (req, res) => {
-    TeacherUser.findOne({ _id: req.params.id }, function(err, user) {
-          if (user) {
-                res.send(user)
-          } else {
-                res.status(400).send("not found user")
-          }
-    })
+      TeacherUser.findOne({ _id: req.params.id }, function(err, user) {
+            if (user) {
+                  res.send(user)
+            } else {
+                  res.status(400).send("not found user")
+            }
+      })
 })
 
-router.get("/auth/teacher/login" ,(req, res) => {
+router.get("/auth/teacher/login", (req, res) => {
       TeacherUser.findOne({ username: req.body.username }, function(err, user) {
-            if(user) {
-                  bcrypt.compare(req.body.password, user.password, function(err, res1) {
-                        if(res1) {
-                              res.send("login success")
+            if (user) {
+                  bcrypt.compare(req.body.password, user.password, function(
+                        err,
+                        res1
+                  ) {
+                        if (res1) {
+                              res.send(
+                                    jwt.encode(
+                                          req.body.username,
+                                          "MY_SECRET_KEY"
+                                    )
+                              )
                         } else {
-                              res.sendStatus(401)           
+                              res.sendStatus(401)
                         }
-                    })
+                  })
             } else {
                   res.sendStatus(401)
+            }
+      })
+})
+
+router.post("/auth/teacher/signup", (req, res) => {
+      TeacherUser.find({}, function(err, result) {
+            if (
+                  result.filter(
+                        teacher =>
+                              teacher.username === req.body.username ||
+                              teacher.email === req.body.email
+                  ).length === 0
+            ) {
+                  let instance = new TeacherUser({
+                        username: req.body.username,
+                        password: req.body.password,
+                        email: req.body.email,
+                        subjectID: []
+                  })
+                  instance.save(function(err2, result2) {
+                        if (err2 || !result2) {
+                              res.sendStatus(400)
+                        } else {
+                              res.send("signup success")
+                        }
+                  })
+            } else {
+                  res.status(400).send("duplicate username or password")
             }
       })
 })
